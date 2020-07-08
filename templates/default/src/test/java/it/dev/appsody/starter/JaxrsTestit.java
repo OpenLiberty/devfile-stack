df1ae0b5f634b480ad4bbb2edf7fc5ee5d2432fc
Mike Andrasak <andrasak@us.ibm.com>
2020-07-08 12:54:31 -0400

    Issue 17 add Microshed testing to java-openliberty odo starter

--- templates/default/src/test/java/it/dev/appsody/starter/JaxrsTestit.java
+++ templates/default/src/test/java/it/dev/appsody/starter/JaxrsTestit.java
@@ -1,3 +1,20 @@
+// tag::copyright[]
+/*******************************************************************************
+Copyright (c) 2020 IBM Corporation and others
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*******************************************************************************/
+// end::copyright[]
 package it.dev.appsody.starter;
 
 
@@ -17,8 +34,10 @@ public class JaxrsTestit {
 	
 	@Container
     public static ApplicationContainer app = new ApplicationContainer()
-                    .withAppContextRoot("/")
-                    .withReadinessPath("/health/ready");
+                    .withAppContextRoot("/starter")
+                    .withReadinessPath("/health/ready")
+                    .withEnv("DEFAULT_HTTP_PORT", "9080")
+	                .withEnv("DEFAULT_HTTPS_PORT", "9443");
                     
 	
 	@RESTClient 
