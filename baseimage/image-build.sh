6712e80c282f6932c66cb0bf3732ec45c5513922
Andrew J. Mauer <ajmauer@gmail.com>
2020-09-01 17:58:20 -0400

    inc image version to 0.1.12

--- baseimage/image-build.sh
+++ baseimage/image-build.sh
@@ -1,1 +1,1 @@
-docker build -t ajymau/java-openliberty-odo -t ajymau/java-openliberty-odo:0.1.1 -f ./Dockerfile .
+docker build -t ajymau/java-openliberty-odo -t ajymau/java-openliberty-odo:0.1.2 -f ./Dockerfile .
