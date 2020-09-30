21303f8dca8d018bcdaecb54040bbcdbf531a987
Andrew J. Mauer <ajmauer@gmail.com>
2020-09-30 12:08:51 -0400

    enhance the mvn cache in stack image with new/updated dependencies
    Signed-off-by: Andrew J. Mauer <ajmauer@gmail.com>

--- baseimage/image-build.sh
+++ baseimage/image-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t ajymau/java-openliberty-odo -t ajymau/java-openliberty-odo:0.1.3 --build-arg stacklabel=$1 -f ./Dockerfile .
+docker build -t ajymau/java-openliberty-odo -t ajymau/java-openliberty-odo:0.1.4 --build-arg stacklabel=$1 -f ./Dockerfile .
