2ab98524f0ac0c869caefc02db0674abedaddda2
Scott Kurz <skurz@us.ibm.com>
2020-10-02 10:14:17 -0400

    Update build script
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- baseimage/image-build.sh
+++ baseimage/image-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t ajymau/java-openliberty-odo -t ajymau/java-openliberty-odo:0.2.0 --build-arg stacklabel=$1 -f ./Dockerfile .
+docker build -t ajymau/java-openliberty-odo -t ajymau/java-openliberty-odo:0.2 -t ajymau/java-openliberty-odo:0.2.2 --build-arg stacklabel=$1 -f ./Dockerfile .
