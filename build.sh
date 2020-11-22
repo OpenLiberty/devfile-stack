75ee9c1439c2f5c6103a8123943e73ea51a7168b
Scott Kurz <skurz@us.ibm.com>
2020-11-22 16:46:32 -0500

    Remove outer-loop/latest paths
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ####
 # For more documentation, see:
 #  https://github.com/OpenLiberty/application-stack/wiki/Open-Liberty-Application-Stack-Customization
@@ -21,24 +23,83 @@ OL_UBI_IMAGE="${OL_UBI_IMAGE:-openliberty/open-liberty:20.0.0.10-kernel-java11-o
 #
 # The name and tag of the "stack image you will build.  This will used to create your inner loop development containers, and also as the base image for the first stage of your outer loop image build.
 #
-STACK_IMAGE="${STACK_IMAGE:-openliberty/application-stack:0.3}"
+STACK_IMAGE="${STACK_IMAGE:-openliberty/application-stack:0.4}"
 
 #
 # URL at which your outer loop Dockerfile is hosted
 #
-DEVFILE_DOCKERFILE_LOC="${DEVFILE_DOCKERFILE_LOC:-https://raw.githubusercontent.com/OpenLiberty/application-stack/master/outer-loop/0.3/Dockerfile}"
+DEVFILE_DOCKERFILE_LOC="${DEVFILE_DOCKERFILE_LOC:-https://raw.githubusercontent.com/OpenLiberty/application-stack-registry/releases/outer-loop/0.4/Dockerfile}"
 
 #
 # URL at which your outer loop deploy YAML template is hosted
 #
-DEVFILE_DEPLOY_YAML_LOC="${DEVFILE_DEPLOY_YAML_LOC:-https://raw.githubusercontent.com/OpenLiberty/application-stack/master/outer-loop/0.3/app-deploy.yaml}"
+DEVFILE_DEPLOY_YAML_LOC="${DEVFILE_DEPLOY_YAML_LOC:-https://raw.githubusercontent.com/OpenLiberty/application-stack-registry/releases/outer-loop/0.4/app-deploy.yaml}"
 
-# Base customization.
-sed -e "s!{{.OL_RUNTIME_VERSION}}!$OL_RUNTIME_VERSION!; s!{{.STACK_IMAGE}}!$STACK_IMAGE!; s!{{.DEVFILE_DOCKERFILE_LOC}}!$DEVFILE_DOCKERFILE_LOC!; s!{{.DEVFILE_DEPLOY_YAML_LOC}}!$DEVFILE_DEPLOY_YAML_LOC!" src/devfile.yaml > devfile.yaml
-sed -e "s!{{.BASE_OS_IMAGE}}!$BASE_OS_IMAGE!; s!{{.OL_RUNTIME_VERSION}}!$OL_RUNTIME_VERSION!" src/stackimage/Dockerfile > stackimage/Dockerfile
+generate() {
 
-# Outer loop customization of Dockerfile
-sed -e "s!{{.STACK_IMAGE}}!$STACK_IMAGE!; s!{{.OL_UBI_IMAGE}}!$OL_UBI_IMAGE!" src/outer-loop/Dockerfile > outer-loop/latest/Dockerfile
+    # Base customization.
+    mkdir -p generated
+    sed -e "s!{{.OL_RUNTIME_VERSION}}!$OL_RUNTIME_VERSION!; s!{{.STACK_IMAGE}}!$STACK_IMAGE!; s!{{.DEVFILE_DOCKERFILE_LOC}}!$DEVFILE_DOCKERFILE_LOC!; s!{{.DEVFILE_DEPLOY_YAML_LOC}}!$DEVFILE_DEPLOY_YAML_LOC!" templates/devfile.yaml > generated/devfile.yaml
+    sed -e "s!{{.BASE_OS_IMAGE}}!$BASE_OS_IMAGE!; s!{{.OL_RUNTIME_VERSION}}!$OL_RUNTIME_VERSION!" templates/stackimage/Dockerfile > generated/stackimage-Dockerfile
 
-# Outer loop copy of app-deploy.yaml (no customization at present)
-cp src/outer-loop/app-deploy.yaml outer-loop/latest/app-deploy.yaml
+    # Outer loop customization of Dockerfile
+    sed -e "s!{{.STACK_IMAGE}}!$STACK_IMAGE!; s!{{.OL_UBI_IMAGE}}!$OL_UBI_IMAGE!" templates/outer-loop/Dockerfile > generated/Dockerfile
+
+}
+
+release() {
+
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    REGISTRY_HOME=$SCRIPT_DIR/../application-stack-registry
+
+    if [ ! -d "$REGISTRY_HOME" ]; then
+        echo "Expecting to find application-stack-registry repo in $REGISTRY_HOME.  Clone this and re-run" 
+        exit 1
+    fi
+
+    RELEASES_DIR=$REGISTRY_HOME/releases
+
+    # copy devfile to releases
+    DIRS="outer-loop/0.4 outer-loop/0.4.0 outer-loop/latest"
+    for d in $DIRS; do 
+        mkdir -p $RELEASES_DIR/$d 
+        cp generated/Dockerfile $RELEASES_DIR/$d 
+        # (no customization at present)
+        cp templates/outer-loop/app-deploy.yaml $RELEASES_DIR/$d
+    done
+
+    # copy devfile to releases
+    DIRS="devfile/0.4 devfile/0.4.0  devfile/latest"
+    mkdir -p $DIRS
+    for d in $DIRS; do 
+        mkdir -p $RELEASES_DIR/$d 
+        cp generated/devfile.yaml $RELEASES_DIR/$d
+    done
+
+    # copy devfile to devfiles (for 'odo create')
+    cp generated/devfile.yaml $REGISTRY_HOME/devfiles/java-openliberty
+    cp templates/meta.yaml $REGISTRY_HOME/devfiles/java-openliberty
+
+    cd $REGISTRY_HOME
+    git add releases devfiles
+    git commit -m "Commit 0.4.0 release artifacts"
+}
+
+
+#set the action, default to generate if none passed.
+ACTION=
+if [ $# -ge 1 ]; then
+  ACTION=$1
+  shift
+fi
+
+case "${ACTION}" in
+  release)
+    generate
+    release
+  ;;
+  *)
+    generate
+  ;;
+esac
+
---
+++ outer-loop/README.md
@@ -0,0 +1,3 @@
+# NOTE
+
+The usage for the paths nested here has been superseded by the 'https:github.com/OpenLiberty/application-stack-registry' repository.  We keep this for now, for backwards compatibility for existing users, but will deprecate/remove.
