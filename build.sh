c3d394899c83ce22c8a774bbb77784806e320a0a
Edward Mezarina <mezarina@us.ibm.com>
2020-11-30 12:48:27 -0500

    Update devfile test execution command and update OL Runtime to 20.0.0.12
    Signed-off-by: Edward Mezarina <mezarina@us.ibm.com>

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ BASE_OS_IMAGE="${BASE_OS_IMAGE:-adoptopenjdk/openjdk11-openj9:ubi}"
 #
 # Version of Open Liberty runtime to use within both inner and outer loops
 #
-OL_RUNTIME_VERSION="${OL_RUNTIME_VERSION:-20.0.0.11}"
+OL_RUNTIME_VERSION="${OL_RUNTIME_VERSION:-20.0.0.12}"
 
 #
 # The Open Liberty base image used in the final stage of the outer loop Dockerfile used to build your application image from
 #
-OL_UBI_IMAGE="${OL_UBI_IMAGE:-openliberty/open-liberty:20.0.0.11-kernel-slim-java11-openj9-ubi}"
+OL_UBI_IMAGE="${OL_UBI_IMAGE:-openliberty/open-liberty:20.0.0.12-kernel-slim-java11-openj9-ubi}"
 
 #
 # The name and tag of the "stack image you will build.  This will used to create your inner loop development containers, and also as the base image for the first stage of your outer loop image build.
@@ -28,13 +28,14 @@ STACK_IMAGE="${STACK_IMAGE:-openliberty/application-stack:0.4}"
 #
 # URL at which your outer loop Dockerfile is hosted
 #
-DEVFILE_DOCKERFILE_LOC="${DEVFILE_DOCKERFILE_LOC:-https://github.com/OpenLiberty/application-stack/releases/download/outer-loop-0.4.0-rc.1/Dockerfile}"
+DEVFILE_DOCKERFILE_LOC="${DEVFILE_DOCKERFILE_LOC:-https://github.com/OpenLiberty/application-stack/releases/download/outer-loop-0.4.0/Dockerfile}"
 
 #
 # URL at which your outer loop deploy YAML template is hosted
 #
-DEVFILE_DEPLOY_YAML_LOC="${DEVFILE_DEPLOY_YAML_LOC:-https://github.com/OpenLiberty/application-stack/releases/download/outer-loop-0.4.0-rc.1/app-deploy.yaml}"
+DEVFILE_DEPLOY_YAML_LOC="${DEVFILE_DEPLOY_YAML_LOC:-https://github.com/OpenLiberty/application-stack/releases/download/outer-loop-0.4.0/app-deploy.yaml}"
 
+# Generates application stack artifacts.
 generate() {
     # Base customization.
     mkdir -p generated
@@ -45,8 +46,7 @@ generate() {
     sed -e "s!{{.STACK_IMAGE}}!$STACK_IMAGE!; s!{{.OL_UBI_IMAGE}}!$OL_UBI_IMAGE!" templates/outer-loop/Dockerfile > generated/Dockerfile
 }
 
-
-#set the action, default to generate if none passed.
+# Execute the specified action. The generate action is the default if none is specified.
 ACTION="generate"
 if [ $# -ge 1 ]; then
     ACTION=$1
@@ -57,7 +57,7 @@ case "${ACTION}" in
         generate
     ;;
     *)
-    echo "Invalid input action. Allowed values: generate. Default: generate."
+    echo "Invalid input action. Allowed action values: generate. Default: generate."
     exit 1
     ;;
 esac
